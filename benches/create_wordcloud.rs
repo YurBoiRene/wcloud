b109c4b16f5297398a610f4ce2a46208b2b17f28
Lennon Anderson <anderslennon@gmail.com>
2024-01-20 12:55:29 -0500

    Clean up bench

--- benches/create_wordcloud.rs
+++ benches/create_wordcloud.rs
@@ -1,16 +1,8 @@
-use std::{fs, iter};
 use std::collections::HashSet;
-use std::time::Duration;
-use wcloud::{Tokenizer, WordCloud, WordCloudSize, Word, DEFAULT_EXCLUDE_WORDS_TEXT, sat};
-use rand::{Rng, SeedableRng};
-use rand::rngs::StdRng;
-use palette::{Pixel, Srgb, Hsl, IntoColor};
-use image::Rgb;
+use std::fs;
+use wcloud::{Tokenizer, WordCloud, WordCloudSize, DEFAULT_EXCLUDE_WORDS_TEXT};
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
-use rand::seq::SliceRandom;
-use wcloud::sat::to_summed_area_table;
-
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn wcloud(c: &mut Criterion) {
     let mut group = c.benchmark_group("create star wars");
@@ -22,69 +14,32 @@ pub fn wcloud(c: &mut Criterion) {
         .replace("HAN", "Han")
         .replace("LUKE'S", "Luke");
 
-// wordcloud_image.save("examples/custom_colors/a_new_hope.png")
-//     .expect("Unable to save image to examples/a_new_hope.png");
+    group.bench_function("generate word cloud", |b| {
+        b.iter(|| {
+            let mut filter = DEFAULT_EXCLUDE_WORDS_TEXT.lines().collect::<HashSet<_>>();
 
-    group.bench_function("generate wordcloud", |b| b.iter(|| {
-        let mut filter = DEFAULT_EXCLUDE_WORDS_TEXT.lines()
-            .collect::<HashSet<_>>();
+            filter.insert("int");
+            filter.insert("ext");
 
-        filter.insert("int");
-        filter.insert("ext");
+            let tokenizer = Tokenizer::default()
+                .with_max_words(1000)
+                .with_filter(filter);
 
-        let tokenizer = Tokenizer::default()
-            .with_max_words(1000)
-            .with_filter(filter);
+            let cloud = WordCloud::default()
+                .with_tokenizer(tokenizer)
+                .with_word_margin(10)
+                .with_rng_seed(1);
 
-        let wordcloud = WordCloud::default()
-            .with_tokenizer(tokenizer)
-            .with_word_margin(10)
-            .with_rng_seed(1);
+            let mask_path = "examples/custom_colors/stormtrooper_mask.png";
+            let mask_image = image::open(mask_path).unwrap().to_luma8();
+            let mask = WordCloudSize::FromMask(mask_image);
 
-        let mask_path = "examples/custom_colors/stormtrooper_mask.png";
-        let mask_image = image::open(mask_path).unwrap().to_luma8();
-        let mask = WordCloudSize::FromMask(mask_image);
-
-        let color_func = |_word: &Word, rng: &mut StdRng| {
-            let lightness = rng.gen_range(0.4..1.0);
-
-            let col = Hsl::new(0.0, 0.0, lightness);
-            let rgb: Srgb = col.into_color();
-
-            let raw: [u8; 3] = rgb.into_format()
-                .into_raw();
-
-            Rgb(raw)
-        };
-
-        wordcloud.generate_from_text_with_color_func(&script_text, mask, black_box(1.0), color_func)
-    }));
+            cloud.generate_from_text(&script_text, mask, black_box(1.0))
+        })
+    });
 
     group.finish();
 }
 
-pub fn sat(c: &mut Criterion) {
-    let mut group = c.benchmark_group("summed area table");
-    group.sample_size(10);
-
-    let table_sizes = [(10, 10), (100, 100), (1000, 1000), (1920, 1080), (2560, 1440), (3840, 2160)];
-    let mut rng = StdRng::seed_from_u64(1);
-
-    for size in table_sizes {
-        group.throughput(Throughput::Bytes(size.0 * size.1));
-        group.bench_with_input(BenchmarkId::from_parameter(size.0 * size.1), &(size.0 * size.1), |b, &table_len| {
-            let mut table: Vec<u32> = (0..table_len).map(|_| rng.gen_range(0..=255))
-                .collect();
-            b.iter(|| sat::to_summed_area_table(&mut table, size.0 as usize, 0));
-        });
-    }
-
-    group.finish();
-}
-
-criterion_group!(
-    benches,
-    wcloud,
-    sat,
-);
+criterion_group!(benches, wcloud);
 criterion_main!(benches);
